2856b6b20142376f1952b21ee8802808b2267ad1
Loicbrassart62 <168406884+Loicbrassart62@users.noreply.github.com>
2024-07-20 14:07:47 +0200

    Update navbar.php

--- Librairies/navbar.php
+++ Librairies/navbar.php
@@ -12,17 +12,17 @@
         </button>
         <ul class="nav-menu">
             <li class="nav-item"><a href="index.php">Accueil</a></li>
-            <li class="nav-item"><a href="Services.php">Services</a></li>
+            <li class="nav-item"><a href="services.php">Services</a></li>
             <li class="nav-item dropdown">
                 <a href="Habitats.php" data-action="dropdown-toggle">Habitats<i
                         class="fa-solid fa-chevron-down"></i></a>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="Biome-Savane.php">Biome Savane</a>
-                    <a class="dropdown-item" href="Biome-Marais.php">Biome Marais</a>
-                    <a class="dropdown-item" href="Biome-Jungle.php">Biome Jungle</a>
+                    <a class="dropdown-item" href="biomeSavane.php">Biome Savane</a>
+                    <a class="dropdown-item" href="biomeMarais.php">Biome Marais</a>
+                    <a class="dropdown-item" href="biomeJungle.php">Biome Jungle</a>
                 </div>
             </li>
-            <li class="nav-item"><a href="Contact.php">Contact</a></li>
-            <li class="nav-item"><a href="Connexion.php">Connexion</a></li>
+            <li class="nav-item"><a href="contact.php">Contact</a></li>
+            <li class="nav-item"><a href="connexion.php">Connexion</a></li>
         </ul>
     </nav>
