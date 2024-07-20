a05d7c81ca47380157ad822087ddd097928b4c4d
Loicbrassart62 <168406884+Loicbrassart62@users.noreply.github.com>
2024-07-20 14:53:31 +0200

    Update employe.php

--- employe.php
+++ employe.php
@@ -27,7 +27,7 @@
                 <li class="nav-item dropdown">
                     <a href="Gestion-alimentation-employe.php" data-action="dropdown-toggle">Gestion alimentation</a>
                 </li>
-                <li class="nav-item"><a href="Connexion.php">Déconnexion</a></li>
+                <li class="nav-item"><a href="connexion.php">Déconnexion</a></li>
             </ul>
         </nav>
 
